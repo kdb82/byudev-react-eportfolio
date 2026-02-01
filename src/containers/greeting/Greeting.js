454dfd3cf41478d6d17b63f8e56275f95e383af9
Kaden Bradshaw <kdnbradshaw@gmail.com>
2026-01-31 22:12:36 -0700

    homepage

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -1,5 +1,5 @@
-import React, {useContext} from "react";
-import {Fade} from "react-reveal";
+import React, { useContext } from "react";
+import { Fade } from "react-reveal";
 // import emoji from "react-easy-emoji";
 import "./Greeting.scss";
 import lightAnim from "../../assets/lottie/Welcome.json";
@@ -8,11 +8,11 @@ import darkAnim from "../../assets/lottie/Welcome_dark.json";
 import DisplayLottie from "../../components/displayLottie/DisplayLottie";
 import SocialMedia from "../../components/socialMedia/SocialMedia";
 import Button from "../../components/button/Button";
-import {illustration, greeting} from "../../portfolio";
+import { illustration, greeting } from "../../portfolio";
 import StyleContext from "../../contexts/StyleContext";
 
 export default function Greeting() {
-  const {isDark} = useContext(StyleContext);
+  const { isDark } = useContext(StyleContext);
   if (!greeting.displayGreeting) {
     return null;
   }
@@ -43,7 +43,7 @@ export default function Greeting() {
                 <Button text="Contact me" href="#contact" />
                 {greeting.resumeLink && (
                   <a
-                    href={require("./resume.pdf")}
+                    href={`${process.env.PUBLIC_URL}/resume.pdf`}
                     download="resume.pdf"
                     className="download-link-button"
                   >
