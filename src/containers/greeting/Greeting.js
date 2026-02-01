4a7a7e8ecd3b9bf134f90dcd989a13b186feca17
Kaden Bradshaw <kdnbradshaw@gmail.com>
2026-01-31 21:55:24 -0700

    resume button patch

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -43,8 +43,8 @@ export default function Greeting() {
                 <Button text="Contact me" href="#contact" />
                 {greeting.resumeLink && (
                   <a
-                    href={require("./resume.pdf")}
-                    download="resume.pdf"
+                    href={require("./KadenBradshawResume.pdf")}
+                    download="KadenBradshawResume.pdf"
                     className="download-link-button"
                   >
                     <Button text="Download my resume" />
